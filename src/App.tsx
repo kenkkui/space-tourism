1c24ab20db5f53c31bf25a4c9d8c7f427a33116e
kenkkui <thifirstime@gmail.com>
2024-05-08 22:28:19 +0800

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import * as React from "react";
+import iconLogo from "./assets/shared/logo.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <div id="logo-icon">
+          <img src={iconLogo} alt="Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <span>00 </span>
+                home
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span>00 </span>
+                destination
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span>00 </span>
+                crew
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span>00 </span>
+                technology
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default App;
