2c9813d2de80dfe4095d8133cadf902432142e11
kenkkui <thifirstime@gmail.com>
2024-05-12 12:51:54 +0800

    just a comnmit gotta save

--- src/components/Destination/Destination.tsx
+++ src/components/Destination/Destination.tsx
@@ -1,14 +1,24 @@
+import moon from "../../assets/destination/image-moon.webp";
+import mars from "../../assets/destination/image-mars.webp";
 import europa from "../../assets/destination/image-europa.webp";
+import titan from "../../assets/destination/image-titan.webp";
 
 export default function Destination() {
   return (
     <section className="destination page">
       <div className="destination-content">
         <div className="illustration">
-          <h1>
-            <span>01</span>
-            Pick your destination
-          </h1>
+          <h1>Pick your destination</h1>
+          <img src={moon} alt="Moon" />
+        </div>
+
+        <div className="description">
+          <ul>
+            <li>Moon</li>
+            <li>Mars</li>
+            <li>Europa</li>
+            <li>Titan</li>
+          </ul>
         </div>
       </div>
     </section>
