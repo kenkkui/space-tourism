730f6aa5ce2081b48ed5f7b24afb92fd7e7a5214
kenkkui <kenzlain@gmail.com>
2024-05-13 09:41:39 +0800

    add logic to currDesination in 02 Destination

--- src/components/Destination/Destination.tsx
+++ src/components/Destination/Destination.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 import moon from "../../assets/destination/image-moon.webp";
 import mars from "../../assets/destination/image-mars.webp";
 import europa from "../../assets/destination/image-europa.webp";
 import titan from "../../assets/destination/image-titan.webp";
+import data from "../../data.json";
 
 export default function Destination() {
+  const [currDestination, setCurrDestination] = useState(0);
+
   return (
     <section className="destination page">
       <div className="destination-content">
@@ -14,11 +19,17 @@ export default function Destination() {
 
         <div className="description">
           <ul>
-            <li>Moon</li>
-            <li>Mars</li>
-            <li>Europa</li>
-            <li>Titan</li>
+            {data.destinations.map((item, i) => {
+              return (
+                <li onClick={() => setCurrDestination(i)} key={i}>
+                  {item.name}
+                </li>
+              );
+            })}
           </ul>
+
+          <h1>{data.destinations[currDestination].name}</h1>
+          <p>{data.destinations[currDestination].description}</p>
         </div>
       </div>
     </section>
